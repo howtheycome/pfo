6ff463326478ee47c7e0c02c62b448db362a9674
buszu <krzysztof.buszewicz@gmail.com>
2014-11-15 17:52:10 +0100

    uploading commit

---
+++ js/bar.js
@@ -0,0 +1,12 @@
+var barChartData = {
+  labels : ['16 lat','19 lat','20 lat','21 lat','22 lat','23 lat','24 lat','25 lat','26 lat','27 lat','28 lat','29 lat','30 lat','31 lat','32 lat','33 lat','34 lat','35 lat','36 lat','37 lat','38 lat','39 lat','40 lat','41 lat','42 lat','43 lat','44 lat','45 lat'],
+  datasets : [
+    {
+      fillColor : "rgba(151,187,205,0.5)",
+      strokeColor : "rgba(151,187,205,0.8)",
+      highlightFill : "rgba(151,187,205,0.75)",
+      highlightStroke : "rgba(151,187,205,1)",
+      data: [2,2,1,10,21,13,19,31,35,19,18,16,8,11,4,2,2,7,4,6,2,2,1,1,1,2,2,1]
+    }
+  ]
+};
---
+++ js/radar.js
@@ -0,0 +1,14 @@
+var radarChartData = {
+    labels : ["Biseksualna", "Homoseksualna", "Heteroseksualna"],
+    datasets: [
+      {
+        fillColor: "rgba(191, 97, 106,0.2)",
+        strokeColor: "rgba(191, 97, 106,1)",
+        pointColor: "rgba(191, 97, 106,0.5)",
+        pointStrokeColor: "rgba(191, 97, 106,1)",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(191, 97, 106,1)",
+        data: [15, 32, 196]
+      }
+    ]
+  };
